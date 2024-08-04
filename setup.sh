7957b846470b5579be2fdc9185007c0b0d19c312
tunglinwood <113751333+tunglinwood@users.noreply.github.com>
2024-08-04 11:27:52 +0800

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+#This is a neccesary shell to operate on the Google Colab
+#Please do !setup.sh in the cell and it will execute automatically
+
 pip install detectron2 --extra-index-url https://wheels.myhloli.com
 pip install magic-pdf[full]==0.6.2b1
 git lfs clone https://huggingface.co/wanderkid/PDF-Extract-Kit
+git clone https://github.com/tunglinwood/MinerU.git
