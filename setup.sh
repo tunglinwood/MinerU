ced6366aa79a761b01d85a65a0af918eed22305b
tunglinwood <113751333+tunglinwood@users.noreply.github.com>
2024-08-04 11:58:02 +0800

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -6,4 +6,3 @@
 pip install detectron2 --extra-index-url https://wheels.myhloli.com
 pip install magic-pdf[full]==0.6.2b1
 git lfs clone https://huggingface.co/wanderkid/PDF-Extract-Kit
-git clone https://github.com/tunglinwood/MinerU.git
